e7d062dfcd09c4f6a03516330b9b3e44f09245be
cjmess01 <cjmess01@gmail.com>
2024-12-10 16:31:40 -0600

    Added launch scripts

---
+++ startup_controller.bash
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+source /opt/ros/humble/setup.bash
+export WEBOTS_HOME=/mnt/c/Program\ Files/Webots
+cd f24_robotics
+colcon build
+source install/setup.bash
+ros2 run webots_hw5 webots_hw5_controller 
